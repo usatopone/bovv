95acf7a383c6fa99016b76f4df86d624328af4c4
usatopone <chauahna942@gmail.com>
2026-06-08 17:30:09 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,11 +86,11 @@ if(form){
   bd.style.display='flex';
   function closeGate(){ bd.style.display='none'; bd.remove(); }
   bd.querySelector('#age-yes').addEventListener('click', function(){
-    window.location.href = "https://inf4hub.com/?utm_campaign=zxSxVTCG6M&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "https://orivexa.site/"; // change to your target page
   });
                                                 
   bd.querySelector('#age-no').addEventListener('click', function(){
-    window.location.href = "https://inf4hub.com/?utm_campaign=zxSxVTCG6M&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "https://orivexa.site/"; // change to your target page
   });
 })();
 
